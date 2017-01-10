97d39b4184f4d6a8ca8d06de2598abb4db63e1d5
Giovanni Filaferro <fila95@live.it>
2017-01-10 18:26:18 +0100

    Added Download Progress Methods

--- rebekka-demo/Demo/AppDelegate.swift
+++ rebekka-demo/Demo/AppDelegate.swift
@@ -18,19 +18,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     
-    func application(application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-            // Override point for customization after application launch.
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
             
-            var configuration = SessionConfiguration()
-            configuration.host = "ftp://speedtest.tele2.net"
-            self.session = Session(configuration: configuration)
-            
-            testList()
-            //testDownload()
-            //testUpload()
-            //testCreate()
-            return true
+        var configuration = SessionConfiguration()
+        configuration.host = "ftp://speedtest.tele2.net"
+        configuration.passive = true
+        
+        self.session = Session(configuration: configuration)
+        
+        testList()
+        testDownload()
+        testUpload()
+        testCreate()
+        
+        return true
     }
     
     func testList() {
@@ -41,32 +43,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func testUpload() {
-        if let URL = NSBundle.mainBundle().URLForResource("TestUpload", withExtension: "png") {
-            let path = "/upload/\(NSUUID().UUIDString).png"
-            self.session.upload(URL, path: path) {
-                (result, error) -> Void in
-                print("Upload file with result:\n\(result), error: \(error)\n\n")
-            }
+        if let URL = Bundle.main.url(forResource: "TestUpload", withExtension: "png") {
+            let path = "/upload/\(UUID().uuidString).png"
+            self.session.upload(URL, path: path, progressHandler: { (progress) in
+                print("Progresso: \(progress)")
+            }, completionHandler: { (complete, error) in
+                print("Complete")
+            })
         }
     }
     
     func testDownload() {
-        self.session.download("/1MB.zip") {
-            (fileURL, error) -> Void in
-            print("Download file with result:\n\(fileURL), error: \(error)\n\n")
+        
+        self.session.download("/5MB.zip", progressHandler: { (progress) in
+            print("Progress: \(progress)");
+        }) { (fileURL, error) in
             if let fileURL = fileURL {
                 do {
-                    try NSFileManager.defaultManager().removeItemAtURL(fileURL)
+                    try FileManager.default.removeItem(at: fileURL)
                 } catch let error as NSError {
                     print("Error: \(error)")
                 }
                 
             }
         }
+        
     }
     
     func testCreate() {
-        let name = NSUUID().UUIDString
+        let name = UUID().uuidString
         self.session.createDirectory("/upload/\(name)") {
             (result, error) -> Void in
             print("Create directory with result:\n\(result), error: \(error)")
